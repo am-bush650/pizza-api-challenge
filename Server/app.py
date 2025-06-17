389f14e17504184e345baa885d0ade83f9027110
mwaura mwangi <fmwauramwangi@gmail.com>
2025-06-17 15:29:44 +0300

    Server/app.py

--- Server/app.py
+++ Server/app.py
@@ -15,6 +15,20 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
+
+    #import Blueprints for different parts of the API (modular route structure)
+    from Server.controllers.restaurant_controller import restaurant_bp
+    from Server.controllers.pizza_controller import pizza_bp
+    from Server.controllers.restaurant_pizza_controller import create_restaurant_pizza
+
+
+    #register each Blueprint with the app, so their routes become active
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(create_restaurant_pizza)
+
+
+    #return the configured app instance
     return app
 
 app = create_app()
